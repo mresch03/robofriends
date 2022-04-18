d494f9ec74521a494cf0f21bc7b1ba00e3f54028
Michael Resch <102878929+mresch03@users.noreply.github.com>
2022-04-18 15:13:24 +0200

    uploading project files

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Michael Resch',
+        username: 'Michael03',
+        email: 'michael.resch03@outlook.de'
+    },
+    {
+        id: 2,
+        name: 'my robot friend 2',
+        username: 'robot2',
+        email: 'myrobotfriend2@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'my robot friend 3',
+        username: 'robot3',
+        email: 'myrobotfriend3@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'my robot friend 4',
+        username: 'robot4',
+        email: 'myrobotfriend4@gmail.com'
+    }
+]
